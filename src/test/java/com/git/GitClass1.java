2f9799651adb7d2bd3a50643f7bc1d51b126ce5c
rajeshkhanna0310 <rrajeshkhanvel@gmail.com>
2022-11-09 22:06:53 +0530

    first

---
+++ src/test/java/com/git/GitClass1.java
@@ -0,0 +1,15 @@
+package com.git;
+
+public class GitClass1 {
+	public void method1() {
+		System.out.println("1");
+
+	}
+	public void method2() {
+		System.out.println("2");
+	}
+	public void method3() {
+		System.out.println("3");
+	}
+
+}
