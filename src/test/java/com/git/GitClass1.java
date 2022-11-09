c034a44c1de111d061925bc2194b0dc17147bc5d
rajeshkhanna0310 <rrajeshkhanvel@gmail.com>
2022-11-09 23:18:24 +0530

    third

--- src/test/java/com/git/GitClass1.java
+++ src/test/java/com/git/GitClass1.java
@@ -11,5 +11,18 @@ public class GitClass1 {
 	public void method3() {
 		System.out.println("3");
 	}
+	
+	public void method9() {
+		System.out.println("9");
+	}
+	
+	public void method8() {
+		System.out.println("8");
+	}
+	
+	public void method7() {
+		System.out.println("7");
+	}
+	
 
 }
